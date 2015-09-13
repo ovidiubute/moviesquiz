b42dab19cb1e3dc633415d5655d287e7e8c0ae1b
ovidiu <ovidiu.bute@gmail.com>
2015-09-13 11:08:24 +0300

    TableGenerator for Movie Entity.

--- src/main/java/com/ovi/apps/movieserver/domain/Movie.java
+++ src/main/java/com/ovi/apps/movieserver/domain/Movie.java
@@ -1,17 +1,29 @@
 package com.ovi.apps.movieserver.domain;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Entity
-public class Movie {
+@Table(name = "MOVIES")
+public class Movie implements Serializable {
+    private static final long serialVersionUID = 3107897896655094094L;
 
     @Id
-    @TableGenerator(name = "movie_id", initialValue = 1000, allocationSize = 100)
-    @GeneratedValue(strategy = GenerationType.TABLE, generator = "movie_id")
+    @TableGenerator(name = "MOVIE_GEN",
+            table = "SEQUENCES",
+            pkColumnName = "SEQ_NAME",
+            valueColumnName = "SEQ_NUMBER",
+            pkColumnValue = "MOVIE_SEQUENCE",
+            initialValue = 1000,
+            allocationSize = 100)
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "MOVIE_GEN")
+    @Column(name = "ID")
     private Long id;
 
+    @Column(name = "NAME")
     private String name;
 
+    @Column(name = "RATING")
     @Enumerated(EnumType.ORDINAL)
     private Rating rating;
 
